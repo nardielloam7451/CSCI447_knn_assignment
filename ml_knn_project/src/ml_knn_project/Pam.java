a4db6c6a5775658f6d906eadc2cd3938d9ac29f2
Rob <rob@robs-MBP>
2019-10-09 21:45:51 -0600

    PAM - your nonstick pan solution

--- ml_knn_project/src/ml_knn_project/Pam.java
+++ ml_knn_project/src/ml_knn_project/Pam.java
@@ -1,11 +1,14 @@
 package ml_knn_project;
 
+import java.util.AbstractMap;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Random;
 
 public class Pam extends ENN {
 	private int numClusters; //keeps track over the number of clusters in the array. 
 	private Random r = new Random();//creates a random number generator for selecting the centeroids initially. 
+	private int numberOfClusters;
 	
 	public Pam(ArrayList<ArrayList<Object>> trainingSet, ArrayList<ArrayList<Object>> testSet, int k,
 			ZeroOneLoss zeroOne, MeanSquaredError mse, double validationSetFraction) {
@@ -19,11 +22,11 @@ public class Pam extends ENN {
 		for(int i=0;i<trainingSet.size();i++) {
 			cloneSet.add(trainingSet.get(i));
 		 }
-		super.buildModel();//builds the ENN model, which is necessary to get the cluster numbers. 
+		super.buildENNModel();//builds the ENN model, which is necessary to get the cluster numbers. 
 		numClusters=super.getModelSize();//gets the cluster numbers for the data set.
 		//resets the training set back to the original set.
 		trainingSet=cloneSet;
-		this.makeInitialClusters(trainingSet, numClusters);
+		this.medoidModel(trainingSet, numClusters);
 	}
 	
 	/**
@@ -31,7 +34,7 @@ public class Pam extends ENN {
 	 * @param trainSet the set to draw medoids from
 	 * @param numberOfClusters the number of clusters to make
 	 */
-	public ArrayList<ArrayList<Object>> makeInitialClusters(ArrayList<ArrayList<Object>> trainSet, int numberOfClusters){
+	public ArrayList<ArrayList<Object>> medoidModel(ArrayList<ArrayList<Object>> trainSet, int numberOfClusters){
 		ArrayList<ArrayList<Object>> clusterModel = new ArrayList<ArrayList<Object>>();
 		ArrayList<Integer> initialClusterIndexList = new ArrayList<Integer>();
 		for (int i = 0; i < numberOfClusters; i++) {
@@ -45,5 +48,41 @@ public class Pam extends ENN {
 			clusterModel.add(trainSet.get(initialClusterIndexList.get(i).intValue()));
 		}
 		return clusterModel;
+	}
+	
+	public void setClassifyNumberOfClusters() {
+		ArrayList<ArrayList<Object>> trainingSetClone = cloneModel(trainingSet);
+		numberOfClusters = getModelSize();
+		trainingSet = trainingSetClone;
+	}
+	
+	public void setRegressionNumberOfClusters() {
+		numberOfClusters = (int)(0.25 * trainingSet.size());
+	}
+	
+	public void buildClusteringModel() {
+		ArrayList<ArrayList<Object>> medoidModel = medoidModel(trainingSet, numberOfClusters);
+		AbstractMap< ArrayList<Object>,Integer> medoidMap = new HashMap<ArrayList<Object>,Integer>();
+		Distance euclid = new EuclidianDistance();
+		
+		for (int i = 0; i < trainingSet.size(); i++) {
+			// gets the first nearest neighbor
+			int nearestClusterIndex = medoidModel.indexOf( getNearestNeighbors(1, medoidModel, trainingSet.get(i)).get(0) );
+			medoidMap.put(trainingSet.get(i), nearestClusterIndex);
+		}
+	}
+	
+	private double calculateDistortion() {
+		double distortion = 0;
+		
+		return distortion;
+	}
+	
+	private ArrayList<ArrayList<Object>> cloneModel(ArrayList<ArrayList<Object>> modelToClone) {
+		ArrayList<ArrayList<Object>> clone = new ArrayList<ArrayList<Object>>(modelToClone.size());
+		for (int i = 0; i < modelToClone.size(); i++) {
+			clone.add(modelToClone.get(i));
+		}
+		return clone;
 	}
 }
